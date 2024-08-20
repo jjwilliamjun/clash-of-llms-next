2661abaeada8cb9b3dceb6e845de1fd87d0ef04a
RhiannaHepburn <rmhepburn04@gmail.com>
2024-08-20 16:49:32 +0800

    WIP

--- clash-of-llms/src/views/HomeView.vue
+++ clash-of-llms/src/views/HomeView.vue
@@ -11,6 +11,9 @@
         Upload Excel Files
       </label>
     </div>
+        <div>
+          <button @click="downloadExcel">Download Excel</button>
+        </div>
 
     <ParameterInputForm v-if="inputOption === 'manual'" />
     <FileUploadForm v-else />
@@ -18,6 +21,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import ParameterInputForm from '@/components/ParameterInput.vue';
 import FileUploadForm from '@/components/FileUpload.vue';
 
@@ -30,6 +34,37 @@ export default {
   components: {
     ParameterInputForm,
     FileUploadForm
-  }
+  },
+  methods: {
+    startSimulation() {
+      console.log("Parameters:", this.parameters);
+    },
+    downloadExcel() {
+      axios({
+        url: 'http://localhost:5000/excel_api/export_excel', 
+        method: 'GET',
+        responseType: 'blob', 
+      })
+      .then((response) => {
+        const blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const link = document.createElement('a');
+
+        link.href = window.URL.createObjectURL(blob);
+        const now = new Date();
+        const timestamp = now.getHours() + "_" + now.getMinutes() + "_" + now.getSeconds();
+    
+
+       const excel_file_name = `clash_of_llms_${timestamp}.xlsx`;
+        link.download = excel_file_name; 
+        
+        link.click();
+        
+        window.URL.revokeObjectURL(link.href);
+      })
+      .catch((error) => {
+        console.error('Error downloading the Excel file:', error);
+      });
+    },
+  },
 };
 </script>
