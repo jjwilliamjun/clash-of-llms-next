8508e1d7557687c81abbaa1b004625aba01cd36b
Edwin Tang <yichentang2003@outlook.com>
2024-08-27 13:21:50 +0800

    Fix pylint (#5)
    Fixed pylint

--- clash-of-llms/excel_api/excel_endpoint.py
+++ clash-of-llms/excel_api/excel_endpoint.py
@@ -1,28 +1,28 @@
-import datetime
-from turtle import pd
+"""Endpoint that exports to excel"""
+from datetime import datetime
 from flask import Flask, send_file, jsonify
 from flask_cors import CORS
-from io import BytesIO
-from excel_export import *
+from excel_export import export_data_excel, generate_game_data
 
 app = Flask(__name__)
-game_data = None #This needs to be a global variable 
+GAMEDATA = None #This needs to be a global variable
 CORS(app)
 
 @app.route('/excel_api/export_excel', methods=['GET'])
 def export_excel():
+    """Exports to excel"""
     try:
-        if game_data is None:
+        if GAMEDATA is None:
             return jsonify({"error": "No game data available"}), 400
 
-        excel_file =export_data_excel(game_data)
+        excel_file =export_data_excel(GAMEDATA)
 
         if excel_file is None:
             return jsonify({"error": "Failed to generate the Excel file"}), 500
         now = datetime.now()
         timestamp = now.strftime("%H_%M_%S")
         excel_file_name = f"clash_of_llms_{timestamp}.xlsx"
-    
+
 
         return send_file(
             excel_file,
@@ -31,8 +31,8 @@ def export_excel():
             mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         )
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    except (ImportError, TypeError) as error:
+        return jsonify({"error": str(error)}), 500
 
 if __name__ == '__main__':
     game_data = generate_game_data() #Testing purposes
--- clash-of-llms/excel_api/game_data.py
+++ clash-of-llms/excel_api/game_data.py
@@ -1,19 +1,25 @@
-
-#Useage: 
-# Initialize GameData instance per simulation
-#Initialize GameTurnData instance per turn in simulation. Must call add_entry to at the end of the simulation
+"""Usage:
+- Initialize GameData instance per simulation
+- Initialize GameTurnData instance per turn in simulation. 
+Must call add_entry to at the end of the simulation"""
 
 class GameData:
+    """Object with game data"""
     def __init__(self):
+        """initialisation"""
         self.data = []
 
     def add_entry(self, simulation_data_obj):
+        """New entry"""
         self.data.append(simulation_data_obj.get_turn_data())
 
     def get_results(self):
+        """returning result"""
         return self.data
 class GameTurnData:
+    """Object with game turn data"""
     def __init__(self):
+        """initialisation"""
         self.turn = None
         self.team = None
         self.message_chosen = None
@@ -23,27 +29,35 @@ class GameTurnData:
         self.decreased_alignment = None
 
     def set_turn(self, turn):
+        """Setting turn to self"""
         self.turn = turn
 
     def set_team(self, team):
+        """Setting team"""
         self.team = team
 
     def set_message_chosen(self, message):
+        """Choosing message"""
         self.message_chosen = message
 
     def set_potency(self, potency):
+        """potency if msg"""
         self.potency = potency
 
     def set_energy_level(self, energy_level):
+        """Energy level"""
         self.energy_level = energy_level
 
     def set_increased_alignment(self, increased_alignment):
+        """increase in alignment"""
         self.increased_alignment = increased_alignment
 
     def set_decreased_alignment(self, decreased_alignment):
+        """decrease in alignment"""
         self.decreased_alignment = decreased_alignment
 
     def get_turn_data(self):
+        """Data associated with the turn"""
         return {
             'Turn': self.turn, 
             'Team': self.team, 
@@ -53,4 +67,3 @@ class GameTurnData:
             '% of nodes with increased alignment towards team': self.increased_alignment, 
             '% of nodes with decreased alignment towards team': self.decreased_alignment
         }
-
