365eb15c3aa8d27b3c28b741d01f4d47bdfa6623
Devarsh <devarsh03101@gmail.com>
2024-08-27 10:37:18 +0800

    WIP

--- clash-of-llms/agent_class/blue_agent.py
+++ clash-of-llms/agent_class/blue_agent.py
@@ -1,14 +1,27 @@
 class Blue_agent: 
-    def __init__(self, ai_name, influence_factor, initial_energy, num_msg_gen):
+    def __init__(self, ai_name, influence_factor, initial_energy, num_msg_sent):
         self.name = ai_name
         self.influence_factor = influence_factor
         self.energy_level = initial_energy
-        self.num_msg_gen = num_msg_gen
+        self.num_msg_sent = 0
 
-    def update_energy_level(self, energy_deducted):
-        new_energy_level = self.energy_level - energy_deducted
-        if new_energy_level <= 0 :
-            return 0
+    def update_energy_level(self, energy_cost):
+        """
+        Attempt to generate and send a message. Consumes energy equal to message_cost.
+        """
+        
+        if self.energy_level - energy_cost <= 0 :
+            print(f"{self.name} cannot send the message")
         else:
-            return new_energy_level
-
+            print(f"{self.name} can send the message")
+            self.energy_level - energy_cost
+    def gen_msg():
+        """
+        Generate a message using chatgpt
+        """
+        pass
+    def send_msg():
+        """
+        Send the message and consume energy
+        """
+        pass
