9aa488ce2ba662efb724d69e443a43cc62d97c35
Edwin Tang <yichentang2003@outlook.com>
2024-10-04 17:36:41 +0800

    FIX: cleanup all files in llm_file folder regardless of extension

--- clash-of-llms/cleanup.sh
+++ clash-of-llms/cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-rm -rf flask_app/create_node_network/round_data/*.json # remove custom LLM JSON files
-rm -f flask_app/llm_files/*.json # remove custom LLM JSON files
-echo Cleaned up round data and llm_file files
+rm -f flask_app/create_node_network/round_data/*.json # remove custom LLM JSON files
+rm -f flask_app/llm_api/llm_files/*.* # remove custom LLM files
+echo Cleaned up round_data and llm_file directory files
